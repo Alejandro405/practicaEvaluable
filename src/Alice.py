a3345ddddbb9c0fd402e8396553d25974900ceec
Alejandro405 <80174674+Alejandro405@users.noreply.github.com>
2022-11-21 19:56:40 +0100

    coreccion estilo terminal

--- src/Alice.py
+++ src/Alice.py
@@ -9,7 +9,6 @@ from funciones_rsa import *
 from colorama import Fore, Style
 from constans import *
 
-
 """
 Paso 0 (Inicialización de recursos) : 
     Generar clave pública/privada y almacenar publica en fichero Alice.py
@@ -26,21 +25,16 @@ socket.conectar()
 
 """ 
 Paso 1 (1) : 
-    Generar clave AT para cifrado en AES con TTP 
+    Generar clave AT para cifrado en AES GCM con TTP 
     Enviar clave de sesión KAT: E(['Alice', KAT], Pub_TTP) + S(KAT, Priv_A)
 """
 KAT = funciones_aes.crear_AESKey()
 engineKAT = funciones_aes.iniciarAES_GCM(KAT)
 print("Clave KAT generada : " + KAT.hex())
-
-# JSON opera con listas de strings!!
 aux = json.dumps([A, KAT.hex()])
 
 cifrado = cifrarRSA_OAEP(aux, pubTTP.public_key())
 firma = firmarRSA_PSS(KAT.hex().encode("utf-8"), asimKey)
-
-# print("firma -> " + firma.hex())
-# print("Cofrado -> " + cifrado.hex())
 
 socket.enviar(cifrado)
 socket.enviar(firma)
@@ -49,48 +43,44 @@ print(Fore.CYAN + "[INFO]     Mensaje enviado a TTP con la clave de sesion KAT" 
 
 socket.cerrar()
 
+inp = input(Fore.YELLOW + "¿Ha enviado bob las claves? [Presione cualquier tecla para continuar]" + Style.RESET_ALL)
 
-time.sleep(8)
 """ 
 Paso 2 (3) :
-    Lanzar petición de conexion con B a TTP
+    Lanzar petición de conexión con B a TTP
     Permanecer a la espera de la clave de sesión del TTP
 """
 socket.conectar()
 peticionInicial = json.dumps([A, B])  # formato String
 socket.enviar(peticionInicial.encode("utf-8"))
 
-print(Fore.LIGHTGREEN_EX + "[STATUS]   Peticion de sesión enviada, esperando clave de sesión KAB" + Style.RESET_ALL)
+print(Fore.LIGHTGREEN_EX + "[STATUS]   Petición de sesión enviada, esperando clave de sesión KAB" + Style.RESET_ALL)
 """ 
-Paso 3 (4) : Desencriptar: 
-    KAT->[Ts, KAB, E_KBT(Ts, K AB) = M].
-    E_KBT(...) == cifrado, mac, nonce 
+Paso 3 (4) : 
+    Recibir Mensaje de respuesta de TTP
+    Desencriptar mensaje: KAT->[Ts, KAB, E_KBT(Ts, K AB) = M].
     IMPORTANTE: A inicia el desafío generado por TTP (TS), que B ha de resolver f(n) = TS + 1, donde n = TSs
 """
-# Pasamos a la espera de la respuesta de TTP con la clave KAB: E_KAT(Ts, KAB, E_KBT(TS, KAB) == X)
 
 cifrado = socket.recibir()
 mac = socket.recibir()
 iv = socket.recibir()
 
+textoClaro = Tools.checkMessage_GCM(binascii.hexlify(KAT), iv, cifrado, mac)
 
-# .hex().encode("utf-8")
-textoClaro = funciones_aes.descifrarAES_GCM(binascii.hexlify(KAT), iv, cifrado, mac)
-if not textoClaro:
-    print(Fore.RED + "[ERROR]   Mensaje alterado durante el envío" + Style.RESET_ALL)
-    exit()
-
-# Textoclaro es si o sí un array de bytes
 
 TS_S, KAB_S, cifM_S, macM_S, ivM_S = json.loads(textoClaro)
 print(Fore.CYAN + "[INFO]     Clave de sesión recibida. Estableciendo conexión con Bob" + Style.RESET_ALL)
+
 # ----------------------  Conexiones con TTP finalizadas   ----------------------
+
+
 socket.cerrar()
 
 """ 
 Paso 3.1 (5) : 
-    Enviar X a Bob y mantenerse a la espera de la resolución del desafío
-    exit()
+    Enviar X a Bob 
+    Mantenerse a la espera de la resolución del desafío por parte de Bob
 """
 
 socket = socket_class.SOCKET_SIMPLE_TCP(LOCALHOST, BOB_PORT)
@@ -108,16 +98,16 @@ cifrado, mac, iv = Tools.reciveAESMessage(socket)
 textoClaro = Tools.checkMessage_GCM(bytes.fromhex(KAB_S), iv, cifrado, mac)
 
 if float(textoClaro) != float(TS_S) + 1:
-    print(Fore.RED + "[ERROR]   Desafío no superado")
+    print(Fore.RED + "[ERROR]   Desafío no superado" + Style.RESET_ALL)
     exit()
 
 """ 
 Paso  (7) : 
-    Cifrar mensaje: KAB->[DNI]
+    Cifrar petición de servicio: KAB->[DNI]
     Obtener respuesta final
 """
 
-dni = "12345678x"
+dni = "77431443B"  # DNI Personal -> respuesta esperada "Alejandro Téllez Montiel"
 engineKAB = funciones_aes.iniciarAES_GCM(bytes.fromhex(KAB_S))
 cifrado, mac, iv = funciones_aes.cifrarAES_GCM(engineKAB, dni.encode("utf-8"))
 
@@ -135,5 +125,7 @@ cifrado, mac, iv = Tools.reciveAESMessage(socket)
 textoClaro = Tools.checkMessage_GCM(bytes.fromhex(KAB_S), iv, cifrado, mac)
 
 print("Nombre correspondiente al DNI(" + dni + "): " + textoClaro.decode())
+
+print(Fore.LIGHTGREEN_EX + "[STATUS]   Petición resuelta" + Style.RESET_ALL)
 
 socket.cerrar()
